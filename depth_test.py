1644e99b6f5f434c304a001460a84729d5525310
p0l0satik <denis.cucu@yandex.ru>
2021-11-01 19:22:53 +0300

    using point clouds instead of depth rgb image

--- depth_test.py
+++ depth_test.py
@@ -1,77 +1,124 @@
 import numpy as np
 import cv2
 from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
-from sklearn.cluster import Birch
-from sklearn.cluster import OPTICS
-from sklearn.cluster import SpectralClustering
+import numpy as np
+import open3d as o3d
+import pickle
+import imageio
 
-# 305.270752059 rgb/305.270752059.png 305.255445061 depth/305.255445061.png
-path = "depth_cut/test4/"
-image = cv2.imread(path + '1532.86007349.png')
-mask = cv2.imread(path + '1532.873477782.png')
-overlay = cv2.addWeighted(image, 1, mask, 0.8, 0)
-cv2.imwrite(path + "overlay.png", overlay)
+# Point cloud from depth (by Konstantin)
+def pointcloudify_depth(depth, intrinsics, dist_coeff=[], undistort=True):
+    shape = depth.shape[::-1]
+    
+    if undistort:
+        undist_intrinsics, _ = cv2.getOptimalNewCameraMatrix(intrinsics, dist_coeff, shape, 1, shape)
+        inv_undist_intrinsics = np.linalg.inv(undist_intrinsics)
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-gray_mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-
-for num_c, rows in enumerate(gray_mask):
-    for num_r, col in enumerate(rows):
-        gray_mask[num_c][num_r] = 255 if col > 0 else 0
-
-another = cv2.bitwise_and(gray_image.copy(), gray_mask)
-
-cv2.imwrite(path + "mask_and_depth.png", another)
-
-# clustering = DBSCAN(min_samples = 3, n_jobs=12)
-km = KMeans(n_clusters=3)
-# brc = Birch(threshold=5, n_clusters=2)
-X = []
-for num_c, rows in enumerate(another):
-    for num_r, col in enumerate(rows):
-        X.append([col,])
-# labels = clustering.fit_predict(X)
-labels = km.fit_predict(X)
-# labels = brc.fit_predict(X)
-# labels = OPTICS(min_samples=10).fit_predict(X)
-
-
-uniq = {}
-depth_p = {}
-for pos, a in enumerate(labels):
-    if not (a in uniq):
-        uniq[a] = 0
-        depth_p[a] = X[pos][0]
     else:
-        uniq[a] +=1
+        inv_undist_intrinsics = np.linalg.inv(intrinsics)
+
+    if undistort:
+        # undist_depthi = cv2.undistort(depthi, intrinsics, dist_coeff, None, undist_intrinsics)
+        map_x, map_y = cv2.initUndistortRectifyMap(intrinsics, dist_coeff, None
+                                                  , undist_intrinsics, shape, cv2.CV_32FC1)
+        undist_depth = cv2.remap(depth, map_x, map_y, cv2.INTER_NEAREST)
+
+    # Generate x,y grid for H x W image
+    grid_x, grid_y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
+    grid = np.concatenate([np.expand_dims(grid_x, -1),
+                           np.expand_dims(grid_y, -1)], axis=-1)
+
+    grid = np.concatenate([grid, np.ones((shape[1], shape[0], 1))], axis=-1)
+
+    # To normalized image coordinates
+    local_grid = inv_undist_intrinsics @ grid.reshape(-1, 3).transpose()  # 3 x H * W
+
+    # Raise by undistorted depth value from image plane to local camera space
+    if undistort:
+        local_grid = local_grid.transpose() * np.expand_dims(undist_depth.reshape(-1), axis=-1)
+
+    else:
+        local_grid = local_grid.transpose() * np.expand_dims(depth.reshape(-1), axis=-1)
+        
+    return local_grid.astype(np.float32)
 
 
-print(uniq)
-print(depth_p)
-maxk1 = min(depth_p, key=depth_p.get)
-print("maxk1 ", maxk1)
+def project_pcd_to_depth(pcd, undist_intrinsics, img_size): 
+    I = np.zeros(img_size, np.float32)
+    h, w = img_size
+    points = np.asarray(pcd.points)
+    d = points[:, 2] #np.linalg.norm(points, axis=1)
+    normalized_points = points / np.expand_dims(points[:, 2], axis=1)
+    proj_pcd = np.round(undist_intrinsics @ normalized_points.T).astype(np.int)[:2].T
+    proj_mask = (proj_pcd[:, 0] >= 0) & (proj_pcd[:, 0] < w) & (proj_pcd[:, 1] >= 0) & (proj_pcd[:, 1] < h)
+    proj_pcd = proj_pcd[proj_mask, :]
+    d = d[proj_mask]
+    pcd_image = np.zeros((1080, 1920))
+    pcd_image[proj_pcd[:, 1], proj_pcd[:, 0]] = d
+    return pcd_image
 
-depth_p[maxk1] = 255
+if __name__ == "__main__":
+    path = "data/test2/"
+    mask = imageio.imread(path + 'mask.png')
+    depth = imageio.imread(path + 'depth.png')
+    gray_mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
 
-maxk2 = min(depth_p, key=depth_p.get) 
-print("maxk2 ", maxk2)
-for num_c, rows in enumerate(another):
-    for num_r, col in enumerate(rows):
-        if (labels[num_c * 1920 + num_r]  == maxk2):
-            another[num_c][num_r] = 255
+    with open('bandeja_standard.pickle', 'rb') as config:
+        config_dict = pickle.load(config)
+
+
+    pcd = o3d.geometry.PointCloud()
+    rgb_cnf = np.load('s10_standard_intrinsics(1).npy', allow_pickle=True).item()
+    pcd.points = o3d.utility.Vector3dVector(pointcloudify_depth(depth, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
+    
+
+    mask_pc = o3d.geometry.PointCloud()
+
+    mask_pc.points = o3d.utility.Vector3dVector(pointcloudify_depth(gray_mask, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
+
+    cld = np.asarray(pcd.points)
+    mask = np.asarray(mask_pc.points)
+
+    filtered = []
+    for n, x in enumerate(cld): 
+        if mask[n][2] > 0:
+            filtered.append(cld[n])
+
+
+    f = o3d.geometry.PointCloud()
+    f.points = o3d.utility.Vector3dVector(filtered)
+
+
+    clustering = DBSCAN(eps = 50)
+    labels = clustering.fit_predict(filtered)
+    uniq = {}
+    depth_p = {}
+    for pos, a in enumerate(labels):
+        if not (a in uniq):
+            uniq[a] = 0
+            depth_p[a] = filtered[pos][2]
         else:
-            another[num_c][num_r] = 0
-# print(clustering.labels_)
-# cv2.imwrite("305.255445061.png", another)
-# gray_mask_not_gray = cv2.cvtColor(another, cv2.COLOR_GRAY2RGB)
-# cv2.imwrite("305.255445061_no_gray.png", gray_mask_not_gray)
+            uniq[a] +=1
+    
 
-# another = cv2.addWeighted(gray_mask_not_gray, 1, mask, 0.7, 0)
-# cv2.imwrite("305.255445061_mask.png", another)
+    ready = []
+    #the first cluster is of zero depth so:
+    maxk = min(depth_p, key=depth_p.get)
+    depth_p[maxk] = 1000000
+    maxk = min(depth_p, key=depth_p.get) 
 
-for num_c, rows in enumerate(another):
-    for num_r, col in enumerate(rows):
-        if (another[num_c][num_r] == 0):
-            mask[num_c][num_r] = (0,0,0)
-cv2.imwrite(path + "ready.png", mask)
+    for n, x in enumerate(labels): 
+        if x == maxk:
+            ready.append(filtered[n])
+
+    r = o3d.geometry.PointCloud()
+    r.points = o3d.utility.Vector3dVector(ready)
+    aligned_depth = project_pcd_to_depth(r, rgb_cnf['undist_intrinsics'], depth.shape[:2])
+    for num_c, rows in enumerate(aligned_depth):
+        for num_r, col in enumerate(rows):
+            if (aligned_depth[num_c][num_r] > 0):
+                aligned_depth[num_c][num_r] = 255
+
+    imageio.imwrite(path + "ready.png", (aligned_depth).astype(np.uint16))
+
+
