a25d56d838159a910cd813ad15e1529dfeca4106
p0l0satik <denis.cucu@yandex.ru>
2021-11-09 12:57:44 +0300

    single run split to functions

--- depth_test_single.py
+++ depth_test_single.py
@@ -11,7 +11,8 @@ import os
 import tqdm
 import collections
 import matplotlib.pylab as plt 
-
+import scipy
+import cProfile
 
 # Point cloud from depth (by Konstantin)
 def pointcloudify_depth(depth, intrinsics, dist_coeff=[], undistort=True):
@@ -64,105 +65,112 @@ def project_pcd_to_depth(pcd, undist_intrinsics, img_size):
     pcd_image[proj_pcd[:, 1], proj_pcd[:, 0]] = d
     return pcd_image
 
-if __name__ == "__main__":
+def smooth_depth(depth):
+    MAX_DEPTH_VAL = 1e6
+    KERNEL_SIZE = 3
+    depth[depth == 0] = MAX_DEPTH_VAL
+    smoothed_depth = scipy.ndimage.minimum_filter(depth, KERNEL_SIZE)
+    smoothed_depth[smoothed_depth == MAX_DEPTH_VAL] = 0
+    return smoothed_depth
+
+def initial_filter(gray_mask, depth):
+    for num_c, col in enumerate(gray_mask):
+        for num_r, val in enumerate(col):
+            if val == 0:
+                depth[num_c][num_r] = 0
+
+def convert_pcd(depth, rgb_cnf):
+    pcd = o3d.geometry.PointCloud()
+    
+    pcd.points = o3d.utility.Vector3dVector(pointcloudify_depth(depth, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
+    return pcd
+
+def filter_pcd(pcd):
+    cld = np.asarray(pcd.points)
+
+    filtered = []
+    for x in cld: 
+        if x[0] > 0  or x[1] > 0 or x[2] > 0: 
+            filtered.append(x)
+    return filtered
+
+def count_labels(filtered, labels):
+    uniq = {}
+    depth_p = {}
+    for pos, a in enumerate(labels):
+        if not (a in uniq):
+            uniq[a] = 0
+            depth_p[a] = filtered[pos][2]
+        else:
+            uniq[a] +=1
+    return (uniq, depth_p)
+
+def filter_small_clusters(uniq, depth_p):
+    filtered_depth_p = {}
+
+    for key, dist in depth_p.items():
+        if abs(dist) > 0.0001 and uniq[key] > 1000:
+            filtered_depth_p[key] = dist
+    return filtered_depth_p
+
+def choose_mask_points(labels, filtered, mink):
+    ready = []
+    for n, x in enumerate(labels): 
+        if x == mink:
+            ready.append(filtered[n])
+    return ready
+
+def back_to_png(ready, rgb_cnf, shp):
+    r = o3d.geometry.PointCloud()
+    r.points = o3d.utility.Vector3dVector(ready)
+    aligned_depth = project_pcd_to_depth(r, rgb_cnf['undist_intrinsics'], shp)
+    return aligned_depth
+
+def upd_mask(aligned_depth):
+    for num_c, rows in enumerate(aligned_depth):
+        for num_r, col in enumerate(rows):
+            if (aligned_depth[num_c][num_r] > 0):
+                aligned_depth[num_c][num_r] = 255
+
+def main():
     path = sys.argv[1]
-    with open(os.path.join(path, "annotation.txt")) as asoc:
-        lines = asoc.readlines()
-        for line in lines:
-            s = line.strip().split(" ")
-            mask = imageio.imread(os.path.join(path, s[0]))
-            depth = imageio.imread(os.path.join(path, s[1]))
-            depth2 = cv2.imread(os.path.join(path, s[1]))
+    # s = line.strip()
+    s = "test2"
 
-            gray_mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-            depth_gray = cv2.cvtColor(depth2, cv2.COLOR_RGB2GRAY)
-            only_mask = cv2.bitwise_and(depth_gray.copy(), gray_mask)
-            
-            depth_dist = {}
-            for num_c, col in enumerate(only_mask):
-                for num_r, val in enumerate(col):
-                    if not (val in depth_dist):
-                        depth_dist[val] = 1
-                    else:
-                        depth_dist[val] += 1
-            
-            # plt.plot(depth_dist)
-            del depth_dist[0]
-            lists = sorted(depth_dist.items()) # sorted by key, return a list of tuples
+    rgb_cnf = np.load('s10_standard_intrinsics(1).npy', allow_pickle=True).item()
+    mask = imageio.imread(os.path.join(path, s, "mask.png"))
+    depth = imageio.imread(os.path.join(path, s, "depth.png"))
 
-            x, y = zip(*lists) # unpack a list of pairs into two tuples
-            plt.figure()
-            plt.scatter(x, y)
-            # plt.show()
-            plt.savefig(os.path.join(path, s[1][:6], "dist"))
-            # for k in sorted(depth_dist.items()):
-            #     print(k)
-            # print()
-            # print()
-            # print()
-            # print()
+    gray_mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
 
-            with open('bandeja_standard.pickle', 'rb') as config:
-                config_dict = pickle.load(config)
+    initial_filter(gray_mask, depth)
 
+    pcd = convert_pcd(depth, rgb_cnf)
 
-            pcd = o3d.geometry.PointCloud()
-            rgb_cnf = np.load('s10_standard_intrinsics(1).npy', allow_pickle=True).item()
-            pcd.points = o3d.utility.Vector3dVector(pointcloudify_depth(depth, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
-            
+    filtered = filter_pcd(pcd)
+    
+    clustering = DBSCAN(eps = 50)
+    # clustering = AgglomerativeClustering(n_clusters =None, distance_threshold = 15000)
+    labels = clustering.fit_predict(filtered)
 
-            mask_pc = o3d.geometry.PointCloud()
+    uniq, depth_p = count_labels(filtered, labels)
+    
 
-            mask_pc.points = o3d.utility.Vector3dVector(pointcloudify_depth(gray_mask, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
+    filtered_depth_p = filter_small_clusters(uniq, depth_p)
 
-            cld = np.asarray(pcd.points)
-            mask = np.asarray(mask_pc.points)
+    mink = min(filtered_depth_p, key=depth_p.get)
 
-            filtered = []
-            for n, x in enumerate(cld): 
-                if mask[n][2] > 0:
-                    filtered.append(cld[n])
+    ready = choose_mask_points(labels, filtered, mink)
 
+    aligned_depth = back_to_png(ready, rgb_cnf, depth.shape[:2]) 
+    # aligned_depth = smooth_depth(aligned_depth)
 
-            f = o3d.geometry.PointCloud()
-            f.points = o3d.utility.Vector3dVector(filtered)
+    upd_mask(aligned_depth)
 
-            clustering = DBSCAN(eps = 50)
-            # clustering = AgglomerativeClustering(n_clusters =None, distance_threshold = 5000)
-            labels = clustering.fit_predict(filtered)
-            uniq = {}
-            depth_p = {}
-            for pos, a in enumerate(labels):
-                if not (a in uniq):
-                    uniq[a] = 0
-                    depth_p[a] = filtered[pos][2]
-                else:
-                    uniq[a] +=1
-            
+    imageio.imwrite(os.path.join(path, s, "res.png"), (aligned_depth).astype(np.uint16))
 
-            ready = []
-            #the first cluster is of zero depth so:
-            # print(uniq)
-            # print(depth_p)
-            filtered_depth_p = {}
-
-            for key, dist in depth_p.items():
-                if abs(dist) > 0.0001 and uniq[key] > 1000:
-                    filtered_depth_p[key] = dist
-            mink = min(filtered_depth_p, key=depth_p.get)
-            # print(mink)
-            for n, x in enumerate(labels): 
-                if x == mink:
-                    ready.append(filtered[n])
-
-            r = o3d.geometry.PointCloud()
-            r.points = o3d.utility.Vector3dVector(ready)
-            aligned_depth = project_pcd_to_depth(r, rgb_cnf['undist_intrinsics'], depth.shape[:2])
-            for num_c, rows in enumerate(aligned_depth):
-                for num_r, col in enumerate(rows):
-                    if (aligned_depth[num_c][num_r] > 0):
-                        aligned_depth[num_c][num_r] = 255
-
-            imageio.imwrite(os.path.join(path, s[2]), (aligned_depth).astype(np.uint16))
+if __name__ == "__main__":
+    main()
+    
 
 
