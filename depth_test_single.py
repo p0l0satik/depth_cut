749c98d333b02a7881d4e546c81f93e234bba0d4
p0l0satik <denis.cucu@yandex.ru>
2021-11-30 12:35:41 +0300

    fast mask fixing algorithm

--- depth_test_single.py
+++ depth_test_single.py
@@ -74,25 +74,47 @@ def smooth_depth(depth):
     return smoothed_depth
 
 def initial_filter(gray_mask, depth):
-    for num_c, col in enumerate(gray_mask):
-        for num_r, val in enumerate(col):
-            if val == 0:
-                depth[num_c][num_r] = 0
+    print(type(gray_mask))
+    mask_arr = np.array(gray_mask).reshape(1920*1080, 1)
+    depth_arr = np.array(depth).reshape(1920*1080, 1)
+    depth_filtered_arr = np.fromiter((d  if mask_arr[num] > 0 else 0 for num, d in enumerate(depth_arr)), dtype=depth_arr.dtype)
+    return depth_filtered_arr.reshape(1080, 1920)
+
+def convert_from_depth(depth_arr, rgb_cnf, shp):
+    with open('bandeja_standard.pickle', 'rb') as config:
+        config_dict = pickle.load(config)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pointcloudify_depth(depth_arr, config_dict['depth']['dist_mtx'],
+                                                config_dict['depth']['dist_coef']))
+    T = np.load('azure2s10_standard_extrinsics(1).npy')
+
+    # Align point cloud with depth reference frame
+    pcd.transform(T)
+    # o3d.visualization.draw_geometries([pcd])
+    # Project aligned point cloud to rgb
+    aligned_depth = project_pcd_to_depth(pcd, rgb_cnf['undist_intrinsics'], shp)
+    # print(aligned_depth.shape)
+    # return aligned_depth
+    return aligned_depth
+    
 
 def convert_pcd(depth, rgb_cnf):
     pcd = o3d.geometry.PointCloud()
-    
+    print(depth.shape)
     pcd.points = o3d.utility.Vector3dVector(pointcloudify_depth(depth, rgb_cnf['undist_intrinsics'], undistort=False)) # H * W X 3
+    print(np.asarray(pcd.points).shape)
     return pcd
 
 def filter_pcd(pcd):
     cld = np.asarray(pcd.points)
+    return [(p) for num, p in enumerate(cld) if p[0] > 0  or p[1] > 0 or p[2] > 0]
 
-    filtered = []
-    for x in cld: 
-        if x[0] > 0  or x[1] > 0 or x[2] > 0: 
-            filtered.append(x)
-    return filtered
+def filter_pcd_points(pcd, pcd_mask):
+    cld = np.asarray(pcd.points)
+    cld_mask = np.asarray(pcd_mask.points)
+    print(cld.shape, cld_mask.shape)
+
+    return [(p) for num, p in enumerate(cld) if cld_mask[num][2] > 0 and (p[0] > 0  or p[1] > 0 or p[2] > 0)]
 
 def count_labels(filtered, labels):
     uniq = {}
@@ -135,20 +157,25 @@ def upd_mask(aligned_depth):
 def main():
     path = sys.argv[1]
     # s = line.strip()
-    s = "test2"
+    s = "test3"
 
     rgb_cnf = np.load('s10_standard_intrinsics(1).npy', allow_pickle=True).item()
     mask = imageio.imread(os.path.join(path, s, "mask.png"))
     depth = imageio.imread(os.path.join(path, s, "depth.png"))
-
+    depth_array = np.load(os.path.join(path, s, "pc.npy"), allow_pickle=True)
     gray_mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
 
-    initial_filter(gray_mask, depth)
+    depth_sparce = convert_from_depth(depth_array, rgb_cnf, depth.shape[:2])
 
-    pcd = convert_pcd(depth, rgb_cnf)
+    # depth = initial_filter(gray_mask, depth)
+    pcd = convert_pcd(depth_sparce, rgb_cnf)
+    # pcd = convert_from_depth(depth_array, rgb_cnf, depth.shape[:2])
 
-    filtered = filter_pcd(pcd)
-    
+    pcd_mask = convert_pcd(gray_mask, rgb_cnf)
+    filtered = filter_pcd_points(pcd, pcd_mask)
+
+    # filtered = filter_pcd(pcd)
+
     clustering = DBSCAN(eps = 50)
     # clustering = AgglomerativeClustering(n_clusters =None, distance_threshold = 15000)
     labels = clustering.fit_predict(filtered)
